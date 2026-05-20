4bb5c7c2e530d18c18c422185be91f6274b8bfdf
nithinbharathi <nithinbharathik@outlook.com>
2026-05-20 11:46:45 -0300

    fix linkedin profile search

--- backend/linkedin_profiles.py
+++ backend/linkedin_profiles.py
@@ -1,7 +1,11 @@
 from tavily import TavilyClient
 import os
+from dotenv import load_dotenv
+from googlesearch import search
+
+load_dotenv()
 
 def get_linkedin_profiles (company, num_profiles):
     client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
-    results = client.search(f"\"Software Engineer at {company}\"", search_depth= "advanced", max_results= num_profiles, exact_match=True)
+    results = client.search(f"site:linkedin.com/in Software Engineer {company}", search_depth= "advanced", max_results= num_profiles)
     return results["results"]
